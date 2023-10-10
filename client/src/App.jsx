c8b6f0f3bb4592bb2401406191b8cca69c03d00a
Khadiga Elzaafarani <khadigazaafarani@gmail.com>
2023-10-10 13:38:15 +0300

    added frontend for guest to register as patient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import PatientRegister from './pages/guest/patientRegisterPage';
+import './pages/guest/patientRegisterPage.css'; // Import the CSS file
+
+
 
 // Styles
 import './App.css'
@@ -6,9 +11,14 @@ import './App.css'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className = 'App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/guest/patientRegister" element={<PatientRegister />} />
+        </Routes>
+      </BrowserRouter>
+   
+  </div>
   )
 }
 
---
+++ client/src/Guestregister.js
@@ -0,0 +1,15 @@
+const  Guestregister= () => {
+
+const handleClick =() =>{
+    console.log('hello reg');
+}
+
+    return ( 
+        <div clssName = "guestReg">
+            <h2>Homepage</h2>
+
+        </div>
+     );
+}
+ 
+export default Guestregister;
