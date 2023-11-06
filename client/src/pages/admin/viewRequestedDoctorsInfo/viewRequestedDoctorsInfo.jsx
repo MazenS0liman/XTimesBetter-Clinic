263185fb2ce0ba2826a478115c4385d43a34b5cb
sarahhh12 <142973581+sarahhh122@users.noreply.github.com>
2023-11-06 10:50:56 +0200

    c1.1

--- client/src/pages/admin/viewRequestedDoctorsInfo/viewRequestedDoctorsInfo.jsx
+++ client/src/pages/admin/viewRequestedDoctorsInfo/viewRequestedDoctorsInfo.jsx
@@ -1,9 +1,73 @@
 import React, { useEffect, useState } from 'react';
 
 function ViewRequestedDoctorsInfo() {
-    
       const [requestedDoctors, setRequestedDoctors] = useState([]);
-       
+      const fetchRequestedDoctors = () => {
+        const url = 'http://localhost:5000/admin/viewREQDoctors';
+    
+        fetch(url)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then((data) => {
+            if (data) {
+              setRequestedDoctors(data);
+
+            } else {
+              console.error('Cannot view requested doctors:', data);
+            }
+          })
+          .catch((error) => {
+            console.error('Error viewing doctor information:', error);
+          });
+      };
+    
+      // Function to handle accepting a doctor
+      const acceptDoctor = (doctorId) => {
+        const url = `http://localhost:5000/admin/viewREQDoctors/accept/${doctorId}`;
+    
+        fetch(url, { method: 'GET' })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            // Fetch the updated doctor data again to reflect the changes
+            fetchRequestedDoctors();
+            alert('Doctor accepted successfully');
+
+          })
+          .catch((error) => {
+            console.error('Error accepting doctor:', error);
+            alert('Error accepting doctor');
+
+          });
+      };
+    
+      // Function to handle rejecting a doctor
+      const rejectDoctor = (doctorId) => {
+        const url = `http://localhost:5000/admin/viewREQDoctors/reject/${doctorId}`;
+      
+        fetch(url, { method: 'GET' })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            // Fetch the updated doctor data again to reflect the changes
+            fetchRequestedDoctors();
+            alert('Doctor rejected successfully');
+
+          })
+          .catch((error) => {
+            console.error('Error rejecting doctor:', error);
+            alert('Error rejecting doctor');
+
+          });
+      };
+      
+    
       useEffect(() => {
         const url = `http://localhost:5000/admin/viewREQDoctors`
         // Make an HTTP PATCH request to send the data to the backend using the requestBody
@@ -33,9 +97,13 @@ function ViewRequestedDoctorsInfo() {
             console.error('Error viewing doctor information : ', error);
             console.log('This Requested doctor does not exist');
           });
+          fetchRequestedDoctors();
+
       },[]);
       return (
+   
         <div>
+
           <h1>Requested Doctors List</h1>
           <table>
             <thead>
@@ -65,7 +133,11 @@ function ViewRequestedDoctorsInfo() {
                   <td>{doctor.affiliation}</td>
                   <td>{doctor.educational_background}</td>
                   <td>{doctor.status}</td>
-                  
+                  <td>
+      <button onClick={() => acceptDoctor(doctor._id)}>Accept</button>
+      <br       />
+      <button onClick={() => rejectDoctor(doctor._id)}>Reject</button>
+    </td>
                 </tr>
               ))}
             </tbody>
--- server/controllers/Admin/viewReqDoctorsInfo.js
+++ server/controllers/Admin/viewReqDoctorsInfo.js
@@ -13,6 +13,40 @@ const viewReqDoctorsInfo = asyncHandler(async(req, res)=>{
     }
 
 })
+const acceptDoctor = asyncHandler(async(req, res)=>{
+    const doctorId = req.params.id;
+    try {
+        // Assuming you have a Mongoose model named "Doctor" representing doctors
+        const doctor = await doctorREQsModel.findByIdAndUpdate(doctorId, { status: 'accepted' }, { new: true });
 
+        if (!doctor) {
+            return res.status(404).json({ message: 'Doctor not found' });
+        }
 
-module.exports ={viewReqDoctorsInfo};
+        return res.status(200).json({ message: 'Doctor accepted successfully', doctor });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'An error occurred while accepting the doctor' });
+    }
+})
+
+const rejectDoctor = asyncHandler(async(req, res)=>{
+        const doctorId = req.params.id;
+            try {
+                // Assuming you have a Mongoose model named "Doctor" representing doctors
+                const doctor = await doctorREQsModel.findByIdAndUpdate(doctorId, { status: 'rejected' }, { new: true });
+        
+                if (!doctor) {
+                    return res.status(404).json({ message: 'Doctor not found' });
+                }
+        
+                return res.status(200).json({ message: 'Doctor rejected successfully', doctor });
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).json({ message: 'An error occurred while accepting the doctor' });
+            }
+       
+    
+    });
+
+module.exports ={viewReqDoctorsInfo,acceptDoctor,rejectDoctor};
