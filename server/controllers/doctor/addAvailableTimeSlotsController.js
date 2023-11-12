f40004a6bd0b36f592f701d5b5aaa960865db154
hendhagoug <hend.hagoug@student.guc.edu.eg>
2023-11-13 01:58:39 +0200

    commit

---
+++ server/controllers/doctor/addAvailableTimeSlotsController.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+//const adminModel = require('../../models/Admin');
+//const patientModel = require('../../models/Patient');
+const doctorModel = require('../../models/Doctor');
+const contractModel = require('../../models/Contract');
+const bcrypt = require('bcrypt');
+const addTimeSlot = asyncHandler(async (req, res) => {
+  const myUsername = 'Logy';
+  const contract = await contractModel.findOne({ username: myUsername });
+  const doctor = await doctorModel.findOne({ username: myUsername });
+//console.log(contract);
+if (!doctor) 
+  return res.status(400).json({ message: 'Doctor not found, cannot add time slots', registeredIn: false });
+else if (!contract || !contract.accepted) {
+  return res.status(400).json({ message: 'Contract not accepted, cannot add time slots', registeredIn: false });
+} else if (!doctor) {
+  return res.status(400).json({ message: 'Doctor not found, cannot add time slots', registeredIn: false });
+} else {
+  const newTimeSlots = req.body.availableTimeSlots; // Array of dates
+  const allTimeSlots= await doctorModel.distinct('availabeTimeSlots', {username: myUsername});
+  const found=allTimeSlots.includes(newTimeSlots);
+  console.log(found);
+  if(!found){
+  const updatedDoctor = await doctorModel.findOneAndUpdate(
+    { username: myUsername, 'availableTimeSlots': { $ne: newTimeSlots[0] } },
+    { $push: { availableTimeSlots: newTimeSlots[0] } },
+    { new: true }
+  );
+  res.status(200).json({ message: 'Time slots added successfully', registeredIn: true, updatedDoctor });
+  }
+  else{
+    return res.status(400).json({ message: 'Time slot already exists', registeredIn: false });
+  }
+  }
+});
+  
+
+module.exports= {addTimeSlot};
